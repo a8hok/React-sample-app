6850aa3a39647125b249bea23e2973ce28e7b878
a8hokkumar <ashokkumar.palanisamy@gyanmatrix.com>
2023-03-11 15:48:22 +0530

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import React, {useEffect, useState} from 'react'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App(props) {
-
   const [data, setData] = useState([]);
-
   useEffect(function(){
     fetch('https://randomuser.me/api/?results=5').then(function(response){
       response.json().then(function(result){
@@ -19,15 +17,13 @@ function App(props) {
   return (
     <div className="App">
       <BrowserRouter> 
-          <div className="App"> 
-              <Link to="/">Contact </Link> 
-              <Link to="/about">About</Link> 
-          </div>
-              <Routes>
-                <Route path="/" element={<Contact />} /> 
-                <Route path="/about" element={<About userData={data}/>} /> 
-              </Routes>
-          </BrowserRouter>
+        <Link to="/">Contact </Link> 
+        <Link to="/about">About</Link> 
+        <Routes>
+          <Route path="/" element={<Contact />} /> 
+          <Route path="/about" element={<About userData={data}/>} /> 
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
